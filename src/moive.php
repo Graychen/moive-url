e9d21271a0688a833291a11da41d239e5f303112
陈家辉 <chenjiahui@haolianluo.com>
2020-01-09 14:36:52 +0800

    feat(src,test): 完成设置电影名称的测试

--- src/moive.php
+++ src/moive.php
@@ -3,13 +3,30 @@ namespace graychen\moive;
 use graychen\moive\singletontrait;
 class moive
 {
+  const HOST ="http://www.kuyunzy1.com/";
   private $name;
 
   use singletontrait;
     
-  public function __construct()
+  public function __construct($name)
   {
-      
+      $this->setName($name);
+  }
+
+  public function setName($name)
+  {
+    $this->name=$name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function getUrl($name)
+  {
+    $name = $this->getName();
+    getContent(self::HOST."search.asp");
   }
 
 }
